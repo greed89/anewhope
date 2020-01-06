afb56928c6d06b3db66da589e3a124e6f99971c3
greed89 <nrg319@naver.com>
2020-01-07 00:16:17 +0900

    base add

---
+++ Client/Assets/Scripts/TextManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextManager : MonoBehaviour
+{
+    private static TextManager instance = null;
+
+    private TextManager()
+    {
+        Debug.Log("TextManager Created");
+    }
+
+    public void ShowText()
+    {
+        Debug.Log("TextManager ShowText");
+    }
+
+    public static TextManager Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = new TextManager();
+            return instance;
+        }
+    }
+}
